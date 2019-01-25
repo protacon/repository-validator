f1c670b011af731945ad2fccaa4f1513b71cdb26
Hekku2 <hekku2@gmail.com>
2019-01-25 11:38:44 +0200

    Adding property names to PushData. Maybe fixing serialization, maybe not.

--- ValidationLibrary.AzureFunctions/GitHubDto/PushData.cs
+++ ValidationLibrary.AzureFunctions/GitHubDto/PushData.cs
@@ -1,8 +1,13 @@
+using Newtonsoft.Json;
+
 namespace ValidationLibrary.AzureFunctions.GitHubDto
 {
     public class PushData
     {
+        [JsonProperty(PropertyName = "repository")]
         public Repository Repository { get; set; }
+
+        [JsonProperty(PropertyName = "organization")]
         public Organization Organization { get; set; }
     }
 }
