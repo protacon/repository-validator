33c19c2ed6a10335c07c1f80e4ccc716c8190417
Hekku2 <hekku2@gmail.com>
2019-03-08 09:31:37 +0200

    Added missing file and udpated Microsoft.NET.Sdk.Functions to fix extensions.json generation problem

---
+++ ValidationLibrary.AzureFunctions/Startup.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ValidationLibrary.AzureFunctions;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace ValidationLibrary.AzureFunctions
+{
+    internal class CustomTelemetryInitializer : ITelemetryInitializer
+    {
+        private string version;
+
+        public CustomTelemetryInitializer()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            version = fileVersionInfo.ProductVersion;
+        }
+        
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Component.Version = version;
+        }
+    }
+
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.Services.AddSingleton<ITelemetryInitializer, CustomTelemetryInitializer>();
+        }
+    }
+}
