02a5b9680a1026807835203b8647faa3017fe20e
Hekku2 <hekku2@gmail.com>
2019-03-22 13:07:23 +0200

    Added a mention to release fetcher that it should not return prerelease or draft-releases.

--- ValidationLibrary/Utils/ReleaseVersionFetcher.cs
+++ ValidationLibrary/Utils/ReleaseVersionFetcher.cs
@@ -18,6 +18,7 @@ namespace ValidationLibrary.Utils
 
         public async Task<string> GetLatest()
         {
+            // Per documentation, this should not return prerelease or draft-releases.
             var result = await _client.Repository.Release.GetLatest(_owner, _name);
             return result.TagName;
         }
