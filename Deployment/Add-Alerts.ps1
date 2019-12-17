71f616f3ace2ceafc43d7d2f5446cb2d0af2b01e
Hekku2 <hekku2@gmail.com>
2019-12-17 13:58:31 +0200

    Parameters to array

--- Deployment/Add-Alerts.ps1
+++ Deployment/Add-Alerts.ps1
@@ -4,22 +4,38 @@ param(
     [Parameter(Mandatory = $true)][string]$ResourceGroup,
     [Parameter(Mandatory = $true)][string]$SiteName
 )
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
 
-$alertTargetActual = Get-AzActionGroup -ResourceGroupName $AlertTargetResourceGroup -Name $AlertTargetGroupName
-$alertRef = New-AzActionGroup -ActionGroupId $alertTargetActual.Id
+$alertParameters = @(
+    [PSCustomObject]@{
+        Name         = 'Bad requests'
+        Description  = 'Too many bad requests received'
+        MetricName   = 'Http4xx'
+        ResourceName = 'Microsoft.Web/Sites'
+        ResourceType = $SiteName
+    }
+)
 
-$badRequests = New-AzMetricAlertRuleV2Criteria -MetricName "Http4xx" -TimeAggregation Total -Operator GreaterThan -Threshold 5
-$site = Get-AzResource -Name $SiteName -ResourceType 'Microsoft.Web/Sites'
-
-Add-AzMetricAlertRuleV2 `
-    -Name 'TestAlert' `
-    -ResourceGroupName $ResourceGroup `
-    -WindowSize 0:5 `
-    -Frequency 0:5 `
-    -TargetResourceScope $site.ResourceId `
-    -TargetResourceType $site.ResourceType `
-    -TargetResourceRegion "northeurope" `
-    -Description "This is description" `
-    -Severity 4 `
-    -ActionGroup $alertRef `
-    -Condition $badRequests
+Write-Host 'Creating alerts'
+Foreach ($alertParameter in $alertParameters) {
+    $alertTargetActual = Get-AzActionGroup -ResourceGroupName $AlertTargetResourceGroup -Name $AlertTargetGroupName
+    $alertRef = New-AzActionGroup -ActionGroupId $alertTargetActual.Id
+    
+    $badRequests = New-AzMetricAlertRuleV2Criteria -MetricName $alertParameter.MetricName -TimeAggregation Total -Operator GreaterThan -Threshold 5
+    $site = Get-AzResource -Name $alertParameter.ResourceName -ResourceType $alertParameter.ResourceType
+    
+    Add-AzMetricAlertRuleV2 `
+        -Name 'TestAlert' `
+        -ResourceGroupName $ResourceGroup `
+        -WindowSize 0:5 `
+        -Frequency 0:5 `
+        -TargetResourceScope $site.ResourceId `
+        -TargetResourceType $site.ResourceType `
+        -TargetResourceRegion "northeurope" `
+        -Description "This is description" `
+        -Severity 4 `
+        -ActionGroup $alertRef `
+        -Condition $badRequests
+}
+Write-Host 'Alerts created'
