456529d932e2b6cc058a6269e9db90983107c3f4
Hekku2 <hekku2@gmail.com>
2020-02-12 12:08:18 +0200

    Improve documentation generation to support multiline comments better

--- ValidationLibrary.MarkdownGenerator/XmlDocumentComment.cs
+++ ValidationLibrary.MarkdownGenerator/XmlDocumentComment.cs
@@ -3,6 +3,6 @@ namespace ValidationLibrary.MarkdownGenerator
     public class XmlDocumentComment
     {
         public string MemberName { get; set; }
-        public string Summary { get; set; }
+        public string[] Summary { get; set; }
     }
 }
