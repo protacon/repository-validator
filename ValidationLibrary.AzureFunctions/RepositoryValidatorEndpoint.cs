ffa39aaca8daa6c1abe7463309ba2b1cb45f5cbe
andrew <andrewpcfield@gmail.com>
2020-03-26 14:53:08 +0200

    Making some static

--- ValidationLibrary.AzureFunctions/RepositoryValidatorEndpoint.cs
+++ ValidationLibrary.AzureFunctions/RepositoryValidatorEndpoint.cs
@@ -15,36 +15,36 @@ namespace ValidationLibrary.AzureFunctions
 {
     public class RepositoryValidatorEndpoint
     {
-        private readonly ILogger<RepositoryValidatorEndpoint> _logger;
         private readonly IGitHubClient _gitHubClient;
         private readonly IValidationClient _validationClient;
         private readonly IGitHubReporter _gitHubReporter;
 
-        public RepositoryValidatorEndpoint(ILogger<RepositoryValidatorEndpoint> logger, IGitHubClient gitHubClient, IValidationClient validationClient, IGitHubReporter gitHubReporter)
+        public RepositoryValidatorEndpoint(IGitHubClient gitHubClient, IValidationClient validationClient, IGitHubReporter gitHubReporter)
         {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _gitHubClient = gitHubClient ?? throw new ArgumentNullException(nameof(gitHubClient));
             _validationClient = validationClient ?? throw new ArgumentNullException(nameof(validationClient));
             _gitHubReporter = gitHubReporter ?? throw new ArgumentNullException(nameof(gitHubReporter));
         }
 
         [FunctionName(nameof(RepositoryValidatorTrigger))]
-        public async Task<HttpResponseMessage> RepositoryValidatorTrigger([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req, [DurableClient] IDurableOrchestrationClient starter)
+        public static async Task<HttpResponseMessage> RepositoryValidatorTrigger(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req, [DurableClient] IDurableOrchestrationClient starter,
+            ILogger logger)
         {
-            _logger.LogDebug("Repository validation hook launched.");
+            logger.LogDebug("Repository validation hook launched.");
             if (starter is null) throw new ArgumentNullException(nameof(starter), "Durable orchestration client was null. Error using durable functions.");
             if (req == null || req.Content == null) throw new ArgumentNullException(nameof(req), "Request content was null. Unable to retrieve parameters.");
 
             var content = await req.Content.ReadAsAsync<PushData>().ConfigureAwait(false);
-            _logger.LogDebug("Request json valid.");
+            logger.LogDebug("Request json valid.");
             var instanceId = await starter.StartNewAsync(nameof(RunOrchestrator), content).ConfigureAwait(false);
 
-            _logger.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            logger.LogInformation($"Started orchestration with ID = '{instanceId}'.");
             return starter.CreateCheckStatusResponse(req, instanceId);
         }
 
         [FunctionName(nameof(RunOrchestrator))]
-        public async Task<StatusCodeResult> RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        public static async Task<StatusCodeResult> RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
         {
             if (context is null) throw new ArgumentNullException(nameof(context), "Durable orchestration context was null. Error running the orchestrator.");
 
@@ -53,34 +53,34 @@ namespace ValidationLibrary.AzureFunctions
         }
 
         [FunctionName(nameof(RunActivity))]
-        public async Task<StatusCodeResult> RunActivity([ActivityTrigger] PushData content)
+        public async Task<StatusCodeResult> RunActivity([ActivityTrigger] PushData content, ILogger logger)
         {
             try
             {
-                _logger.LogDebug("Executing validation activity.");
+                logger.LogDebug("Executing validation activity.");
                 if (content is null) throw new ArgumentNullException(nameof(content), "No content to execute the activity.");
 
                 ValidateInput(content);
 
-                _logger.LogInformation("Doing validation. Repository {owner}/{repositoryName}", content.Repository?.Owner?.Login, content.Repository?.Name);
+                logger.LogInformation("Doing validation. Repository {owner}/{repositoryName}", content.Repository?.Owner?.Login, content.Repository?.Name);
 
                 await _validationClient.Init().ConfigureAwait(false);
                 var report = await _validationClient.ValidateRepository(content.Repository.Owner.Login, content.Repository.Name, false).ConfigureAwait(false);
 
-                _logger.LogDebug("Sending report.");
+                logger.LogDebug("Sending report.");
                 await _gitHubReporter.Report(new[] { report }).ConfigureAwait(false);
 
-                _logger.LogDebug("Performing auto fixes.");
+                logger.LogDebug("Performing auto fixes.");
                 await PerformAutofixes(report).ConfigureAwait(false);
 
-                _logger.LogInformation("Validation finished");
+                logger.LogInformation("Validation finished");
                 return new OkResult();
             }
             catch (Exception exception)
             {
                 if (exception is ArgumentException)
                 {
-                    _logger.LogError(exception, "Invalid request received");
+                    logger.LogError(exception, "Invalid request received");
 
                     return new BadRequestResult();
                 }
