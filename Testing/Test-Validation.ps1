c96dc4802edfe64867ccea2e84bf6c00fee0661d
andrew <andrewpcfield@gmail.com>
2020-04-20 20:56:37 +0300

    Remove settings file because of jenkins file

--- Testing/Test-Validation.ps1
+++ Testing/Test-Validation.ps1
@@ -6,32 +6,33 @@
     This functions can be used the test Azure Functions validation endpoint
     without making changes in GitHub. This is also used as the last step of
     production deployment to warm up the app and verify that validation
-    endpoint returns correct answer
+    endpoint returns correct answer.
+
+    .PARAMETER ResourceGroup
+    Resource group name
+
+    .PARAMETER WebAppName
+    Name of the web app
 
     .PARAMETER Organization
     Organization/user containing the repository (default by-pinja)
 
     .PARAMETER Repository
     Name of the repository to be validated  (default repository-validator)
-
-    .PARAMETER SettinsFile
-    Settings file that contains environment settings.
-    Defaults to 'developer-settings.json'
 #>
 [CmdLetBinding()]
 param(
+    [Parameter(Mandatory)][string]$ResourceGroup,
+    [Parameter()][string]$WebAppName = $ResourceGroup,
     [Parameter()][string]$Organization = 'by-pinja',
-    [Parameter()][string]$Repository = 'repository-validator',
-    [Parameter()][string]$SettingsFile = 'developer-settings.json'
+    [Parameter()][string]$Repository = 'repository-validator'
 )
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-Write-Host "Reading settings from file $SettingsFile"
-$settingsJson = Get-Content -Raw -Path $SettingsFile | ConvertFrom-Json
-
 $validationAddress = ./Deployment/Get-FunctionUri.ps1 `
-    -ResourceGroup $settingsJson.ResourceGroupName `
+    -ResourceGroup $ResourceGroup `
+    -WebAppName $WebAppName `
     -FunctionName 'RepositoryValidatorTrigger'
 
 # This should match the webhook content sent by github, but we are only using
@@ -49,7 +50,8 @@ Write-Host 'Send validation request'
 Invoke-RestMethod -Method POST -Uri $validationAddress -Body $params -ContentType 'application/json;charset=UTF-8'
 
 $statusCheckAddress = ./Deployment/Get-FunctionUri.ps1 `
-    -ResourceGroup $settingsJson.ResourceGroupName `
+    -ResourceGroup $ResourceGroup `
+    -WebAppName $WebAppName `
     -FunctionName 'StatusCheck'
 
 Write-Host 'Send status check request'
