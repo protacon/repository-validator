725d6c9d6b9380da401b6cc4516fb472d577e79a
andrew <andrewpcfield@gmail.com>
2020-04-20 19:23:16 +0300

    Remove setting from script

--- Deployment/Create-RunSettingsFile.ps1
+++ Deployment/Create-RunSettingsFile.ps1
@@ -5,23 +5,24 @@
     .DESCRIPTION
     The Azure environment should already exist and the webapp already deployed.
 
-    .PARAMETER SettinsFile
-    Settings file that contains resource group name, web app name etc. Defaults to 'developer-settings.json'
+    .PARAMETER ResourceGroup
+    Name of the resource group that has the web app deployed.
+    
+    .PARAMETER WebAppName
+    Name of the target web app. If not set, resource group name is used.
 #>
 param(
-    [Parameter()][string]$SettingsFile = 'developer-settings.json'
+    [Parameter(Mandatory)][string]$ResourceGroup,
+    [Parameter()][string]$WebAppName = $ResourceGroup
 )
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-Write-Host "Reading settings from file $SettingsFile"
-$settingsJson = Get-Content -Raw -Path $SettingsFile | ConvertFrom-Json
-
 . "./Deployment/FunctionUtil.ps1"
 
 Write-Host "Fetch credentials..."
-$kuduCreds = Get-KuduCredentials $settingsJson.ResourceGroupName $settingsJson.ResourceGroupName
-$code = Get-DefaultKey -AppName $settingsJson.ResourceGroupName -EncodedCreds $kuduCreds
+$kuduCreds = Get-KuduCredentials $WebAppName $ResourceGroup
+$code = Get-DefaultKey $WebAppName $kuduCreds
 
 [xml]$document = New-Object System.Xml.XmlDocument
 $declaration = $document.CreateXmlDeclaration('1.0', 'UTF-8', $null)
@@ -34,7 +35,7 @@ $root.AppendChild($parameters)
 
 $appNameNode = $document.CreateElement('Parameter')
 $appNameNode.SetAttribute('name', 'FunctionAppName')
-$appNameNode.SetAttribute('value', $settingsJson.ResourceGroupName)
+$appNameNode.SetAttribute('value', $WebAppName)
 $parameters.AppendChild($appNameNode);
 
 $codeNode = $document.CreateElement('Parameter')
