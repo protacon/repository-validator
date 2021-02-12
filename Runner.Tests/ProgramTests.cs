05c12608c4b12957360efa010ad1a21f7336fdb1
Heikki-Jussi Niemi <hekku2@gmail.com>
2021-02-12 13:35:09 +0200

    Improve documentation generation testing

--- Runner.Tests/ProgramTests.cs
+++ Runner.Tests/ProgramTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
@@ -13,9 +15,32 @@ namespace Runner.Tests
         }
 
         [Test]
-        public async Task Main_DocumentationGeneration()
+        public async Task Main_DocumentationIsGeneratedToCorrectFolderWithSomeContent()
         {
-            await Program.Main(new string[] { "generate-document" });
+            // Guid is used as a folder name to avoid outputting to some existing folder and deleting it.
+            var folderName = Guid.NewGuid().ToString();
+
+            try
+            {
+                await Program.Main(new string[] { "generate-document", "-o", folderName });
+
+                Assert.IsTrue(Directory.Exists(folderName));
+                var fileNames = Directory.GetFiles(folderName);
+
+                Assert.Greater(fileNames.Length, 1, "There should be at least Rules.md and other rules.");
+                Assert.Contains($"{folderName}{Path.DirectorySeparatorChar}Rules.md", fileNames, "There should be at least Rules.md");
+
+                foreach (var fileName in fileNames)
+                {
+                    var lines = File.ReadAllLines(fileName);
+                    Assert.IsTrue(fileName.EndsWith(".md"), "Currently all files should be markdown files.");
+                    Assert.Greater(lines.Length, 0, "There should be some content in the file.");
+                }
+            }
+            finally
+            {
+                Directory.Delete(folderName, true);
+            }
         }
     }
 }
